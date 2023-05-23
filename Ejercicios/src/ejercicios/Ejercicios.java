76649d55b0aac849c3e3301d4a66b260a36e8668
NickLimez <nicklimez93@gmail.com>
2023-05-23 19:29:31 -0300

    primer push de java

---
+++ Ejercicios/src/ejercicios/Ejercicios.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicios {
+
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        char caracter = 'a';
+        int entero = 20;
+        double real_64 = 2.55;
+        boolean logico = true;
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingrese un número");
+        int num3 = leer.nextInt();        
+        
+        entero++;
+        entero--;
+        
+        int num1 = 5;
+        int num2 = 5;
+        
+        if (num1 == num2){ }
+        
+        System.out.print("Ingrese una frase");
+        String c = leer.nextLine();
+        
+}}
